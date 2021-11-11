74c62bcd53188b52aaf7633abc462efd1e5bc9c0
Casimero Tanseco <tansecoc@oregonstate.edu>
2021-11-10 17:15:23 -0800

    added the following tables: lo_customers, lo_genders, lo_activities, lo_products, lo_stores

---
+++ lo_database_ddq.sql
@@ -0,0 +1,300 @@
+-- MySQL dump 10.16  Distrib 10.1.37-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: TBD
+-- ------------------------------------------------------
+-- Server version	10.1.37-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `lo_customers`
+--
+
+DROP TABLE IF EXISTS `lo_customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lo_customers` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `customer_email` varchar NOT NULL,
+  `customer_firstname` varchar,
+  `customer_lastname` varchar,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lo_customers`
+--
+
+LOCK TABLES `lo_customers` WRITE;
+/*!40000 ALTER TABLE `lo_customers` DISABLE KEYS */;
+INSERT INTO `lo_customers` (`customer_id`, `customer_email`, `customer_firstname`, `customer_lastname`) VALUES
+(1, `jkim@gmail.com`, `Jenny`, `Kim`),
+(2, `Paul@gmail.com`, `Paul`, `Smith`);
+/*!40000 ALTER TABLE `lo_customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lo_genders`
+--
+
+DROP TABLE IF EXISTS `lo_genders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lo_genders` (
+  `gender_id` char(1),
+  `gender_description` varchar NOT NULL,
+  PRIMARY KEY (`gender_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lo_genders`
+--
+
+LOCK TABLES `lo_genders` WRITE;
+/*!40000 ALTER TABLE `lo_genders` DISABLE KEYS */;
+INSERT INTO `lo_genders` (`gender_id`, `gender_description`) VALUES
+(`M`, `Male`),
+(`F`, `Female`),
+(`U`, `Unisex`);
+/*!40000 ALTER TABLE `lo_genders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lo_activities`
+--
+
+DROP TABLE IF EXISTS `lo_activities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lo_activities` (
+  `activity_id` int NOT NULL AUTO_INCREMENT,
+  `activity_description` varchar NOT NULL,
+  PRIMARY KEY (`activity_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lo_activities`
+--
+
+LOCK TABLES `lo_activities` WRITE;
+/*!40000 ALTER TABLE `lo_activities` DISABLE KEYS */;
+INSERT INTO `lo_activities` (`activity_id`, `activity_description`) VALUES
+(1, `Running`),
+(2, `Swimming`),
+(3, `Biking`);
+/*!40000 ALTER TABLE `lo_activities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lo_products`
+--
+
+DROP TABLE IF EXISTS `lo_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lo_products` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar NOT NULL,
+  `product_description` varchar,
+  `gender_id` char(1),
+  `activity_id` int,
+  `product_price` decimal(6,2) NOT NULL,
+  PRIMARY KEY (`product_id`),
+  CONSTRAINT `lo_products_fk_1` FOREIGN KEY (`gender_id`) REFERENCES `lo_genders` (`gender_id`),
+  CONSTRAINT `lo_products_fk_2` FOREIGN KEY (`activity_id`) REFERENCES `lo_activities` (`activity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lo_products`
+--
+
+LOCK TABLES `lo_products` WRITE;
+/*!40000 ALTER TABLE `lo_products` DISABLE KEYS */;
+INSERT INTO `lo_products` (`product_id`, `product_name`, `product_description`
+                            `gender_id`, `activity_id`, `product_price`) VALUES
+(1, `Leggings`, `Running tights made of Nylux.`, `F`, `Running`, 98),
+(2, `Vent Tech 2.0 Shirt`, `Tech running shirt made of Nylux.`, `M`, `Running`, 68),
+(3, `Cycle Shirt`, `Tech cycling shirt made of Nylux.`, `F`, `Cycling`, 88);
+/*!40000 ALTER TABLE `lo_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lo_stores`
+--
+
+DROP TABLE IF EXISTS `lo_stores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lo_stores` (
+  `store_id` int NOT NULL AUTO_INCREMENT,
+  `store_email` varchar NOT NULL,
+  `store_phone` varchar NOT NULL,
+  `store_country` varchar NOT NULL,
+  `store_street` varchar NOT NULL,
+  `store_city` varchar NOT NULL,
+  `store_state` varchar,
+  `store_zip` varchar,
+  PRIMARY KEY (`store_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lo_stores`
+--
+
+LOCK TABLES `lo_stores` WRITE;
+/*!40000 ALTER TABLE `lo_stores` DISABLE KEYS */;
+INSERT INTO `lo_stores` (`store_id`, `store_email`, `store_phone`, `store_street`,
+                        `store_city`, `store_state`, `store_country`, `store_zip`) VALUES
+(1, `lulu1@luluorange.com`, `732-517-4632`, `217 Main Ave`, `New York`, `NY`, `USA`, `10001`),
+(2, `lulu2@luluorange.com`,	`604-874-1234`,	`970 Robson St`, `Vancouverk`, `BC`, `Canada`, `V6Z 2E7`);
+/*!40000 ALTER TABLE `lo_stores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+------------------------------------------------------------------------------------------
+--
+--   Placeholder for lo_orders and lo_orders_products tables
+--
+------------------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------------------
+--
+--   The below section includes the bsg tables as an example
+--
+------------------------------------------------------------------------------------------
+
+--
+-- Table structure for table `bsg_cert_people`
+--
+
+DROP TABLE IF EXISTS `bsg_cert_people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bsg_cert_people` (
+  `cid` int(11) NOT NULL DEFAULT '0',
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `certification_date` date NOT NULL,
+  PRIMARY KEY (`cid`,`pid`),
+  KEY `pid` (`pid`),
+  CONSTRAINT `bsg_cert_people_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `bsg_cert` (`certification_id`),
+  CONSTRAINT `bsg_cert_people_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `bsg_people` (`character_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bsg_cert_people`
+--
+
+LOCK TABLES `bsg_cert_people` WRITE;
+/*!40000 ALTER TABLE `bsg_cert_people` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bsg_cert_people` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bsg_people`
+--
+
+DROP TABLE IF EXISTS `bsg_people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bsg_people` (
+  `character_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `homeworld` int(11) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `race` varchar(5) NOT NULL DEFAULT 'Human',
+  PRIMARY KEY (`character_id`),
+  KEY `homeworld` (`homeworld`),
+  CONSTRAINT `bsg_people_ibfk_1` FOREIGN KEY (`homeworld`) REFERENCES `bsg_planets` (`planet_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=159 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bsg_people`
+--
+
+LOCK TABLES `bsg_people` WRITE;
+/*!40000 ALTER TABLE `bsg_people` DISABLE KEYS */;
+INSERT INTO `bsg_people` VALUES (6,'Saul','Tigh',NULL,71,'Human'),(9,'Callandra','Henderson',NULL,NULL,'Human'),(121,'harry','goober',18,23,'Human'),(156,'','',1,0,'Human'),(157,'','',3,0,'Human'),(158,'The','Man',16,22,'Human');
+/*!40000 ALTER TABLE `bsg_people` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bsg_planets`
+--
+
+DROP TABLE IF EXISTS `bsg_planets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bsg_planets` (
+  `planet_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `population` bigint(20) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `capital` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`planet_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bsg_planets`
+--
+
+LOCK TABLES `bsg_planets` WRITE;
+/*!40000 ALTER TABLE `bsg_planets` DISABLE KEYS */;
+INSERT INTO `bsg_planets` VALUES (1,'Gemenon',2800000000,'Old Gemenese','Oranu'),(2,'Leonis',2600000000,'Leonese','Luminere'),(3,'Caprica',4900000000,'Caprican','Caprica City'),(7,'Sagittaron',1700000000,NULL,'Tawa'),(16,'Aquaria',25000,NULL,NULL),(17,'Canceron',6700000000,NULL,'Hades'),(18,'Libran',2100000,NULL,NULL),(19,'Picon',1400000000,NULL,'Queestown'),(20,'Scorpia',450000000,NULL,'Celeste'),(21,'Tauron',2500000000,'Tauron','Hypatia'),(22,'Virgon',4300000000,NULL,'Boskirk');
+/*!40000 ALTER TABLE `bsg_planets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bsg_spaceship`
+--
+
+DROP TABLE IF EXISTS `bsg_spaceship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bsg_spaceship` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `seperate_saucer_section` bit(1) DEFAULT b'0',
+  `length` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bsg_spaceship`
+--
+
+LOCK TABLES `bsg_spaceship` WRITE;
+/*!40000 ALTER TABLE `bsg_spaceship` DISABLE KEYS */;
+INSERT INTO `bsg_spaceship` VALUES (1,'t1','',0),(2,'t2','',0),(3,'t2','',0),(4,'t3','',0),(5,'t4','\0',0),(6,'t5','',0);
+/*!40000 ALTER TABLE `bsg_spaceship` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-03  0:38:33
