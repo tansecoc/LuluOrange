6f13cd75f866b901bf619e2ea4e2d4e058992f83
Casimero Tanseco <tansecoc@oregonstate.edu>
2021-11-24 20:46:04 -0600

    updated formatting on edit and delete buttons

--- nodejs/dbcon.js
+++ nodejs/dbcon.js
@@ -3,12 +3,12 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
 
-  user            : 'cs340_zhaoso',
-  password        : '3818',
-  database        : 'cs340_zhaoso'
+  // user            : 'cs340_zhaoso',
+  // password        : '3818',
+  // database        : 'cs340_zhaoso'
 
-  // user            : 'cs340_tansecoc',
-  // password        : '7695',
-  // database        : 'cs340_tansecoc'
+  user            : 'cs340_tansecoc',
+  password        : '7695',
+  database        : 'cs340_tansecoc'
 });
 module.exports.pool = pool;
